faf6b2b6feeb55b13e1f157388daabdff7ce3476
Temistoclis <temistoclisxp58@gmail.com>
2024-08-19 19:32:07 -0300

    adicionando o teste de comentario

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,34 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
 import PostComment from '.';
+import App from '../../App';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    test('Deve inserir dois comentarios',()=>{
+
+        const {debug} = render(<App/>)
+        
+        fireEvent.change(screen.getByTestId('campo-text'),{
+            target:{
+                value:'legal sua miniatura do batmovel'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        fireEvent.change(screen.getByTestId('campo-text'),{
+            target:{
+                value:'legal seu batmovel preto'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        debug()
+        expect(screen.getByText('legal sua miniatura do batmovel')).toBeInTheDocument()
+        expect(screen.getByText('legal seu batmovel preto')).toBeInTheDocument()
     });
 });
--- src/components/PostComments/index.tsx
+++ src/components/PostComments/index.tsx
@@ -26,8 +26,8 @@ const Post = () => {
                 ))}
             </ul>
             <form onSubmit={handleAddComment} className={styles['post-comments-form']}>
-                <textarea value={tempComment} onChange={e => setTempComment(e.target.value)} required className={styles['post-comments-form-textarea']} />
-                <button type="submit" className={styles['post-comments-form-button']}>
+                <textarea value={tempComment} onChange={e => setTempComment(e.target.value)} required className={styles['post-comments-form-textarea']} data-testid="campo-text"/>
+                <button data-testid="btn-comentar"type="submit" className={styles['post-comments-form-button']}>
                     Comentar
                 </button>
             </form>
